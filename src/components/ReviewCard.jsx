7e0540bae84cac6461dc099776bc1ffd52e584b3
PythoSalaf <alfaheed1010@gmail.com>
2024-11-12 14:00:18 +0100

    new commit

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,10 +1,10 @@
 import { BiSolidQuoteAltLeft } from "react-icons/bi";
 import { Profile } from "../assets";
 
-const ReviewCard = () => {
+const ReviewCard = ({ name, stack, review }) => {
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="w-[250px] md:w-[300px] h-[300px] flex flex-col items-start bg-white shadow-lg  rounded-xl  pb-[1rem] relative">
+      <div className="w-[250px] md:w-[300px] h-[310px] flex flex-col items-start bg-white shadow-lg  rounded-xl  pb-[1rem] relative">
         <div className="w-[90%] mx-auto py-5">
           <h2 className="uppercase flex items-start ">
             <BiSolidQuoteAltLeft />
@@ -12,18 +12,14 @@ const ReviewCard = () => {
               Hardworking
             </span>
           </h2>
-          <p className="my-5">
-            Hardworking developer. In addition to his technical skills, Taoheed
-            is also an excellent communicator and team player. He is always
-            willing to go extra mile to complete any task given to him.
-          </p>
+          <p className="my-5">{review}</p>
           <div className="flex items-center gap-x-4">
-            <div className="rounded-full size-16">
+            <div className="rounded-full size-14">
               <img src={Profile} alt="pro" className="size-full rounded-full" />
             </div>
             <div className="">
-              <h3 className="uppercase text-lg font-semibold">Taoheed</h3>
-              <p className="text-sm">Software Engineer</p>
+              <h3 className="uppercase text-lg font-semibold">{name}</h3>
+              <p className="text-sm">{stack}</p>
             </div>
           </div>
         </div>
--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -1,13 +1,13 @@
-import { ReviewCard } from "../components";
+import { TestSlider } from "../components";
 
 const Testimonial = () => {
   return (
-    <div className="bg-primary py-5">
+    <div className="bg-primary py-5 w-full">
       <h2 className="text-center text-2xl md:text-3xl text-white">
         Testimonials
       </h2>
-      <div className="mt-9">
-        <ReviewCard />
+      <div className="my-9 w-[90%] md:w-[98%] mx-auto">
+        <TestSlider />
       </div>
     </div>
   );
