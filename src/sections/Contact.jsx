1255e7ec17319e63ad94ba6f2f6d562ce536dbbb
PythoSalaf <alfaheed1010@gmail.com>
2024-11-20 18:34:27 +0100

    last section concluded

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
       <h2 className="text-center text-2xl font-semibold md:text-3xl  mb-10">
         Contact
       </h2>
-      <div className="flex items-center flex-col-reverse md:flex-row justify-between">
+      <div className="flex items-start flex-col-reverse md:flex-row justify-between">
         <div className="relative w-full">
           <img
             src={Blob}
@@ -58,14 +58,14 @@ const Contact = () => {
                 <div className="bg-white rounded-lg size-10 md:size-[50px] text-primary flex items-center justify-center">
                   {item.icon}
                 </div>
-                <div className="text-white">
+                <div className="text-white w-full">
                   <span className="text-base capitalize">{item.title}</span>
-                  <p className="text-xl mt-[2px] font-semibold">
+                  <p className=" mt-[2px] font-semibold">
                     <a
                       href={item.link}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="hover:underline text-white"
+                      className="hover:underline text-white text-sm md:text-lg lg:text-xl"
                     >
                       {item.displayText}
                     </a>
@@ -75,7 +75,29 @@ const Contact = () => {
             ))}
           </div>
         </div>
-        <div className="w-full"></div>
+        <div className="w-full mb-14 md:mb-0">
+          <div className="w-[90%] md:w-[80%] mx-auto">
+            <form>
+              <input
+                type="text"
+                placeholder="Name........"
+                className="border border-[#dadada] w-full h-11 md:h-14 rounded-lg px-3"
+              />
+              <input
+                type="text"
+                placeholder="Email........"
+                className="border border-[#dadada] w-full my-4 h-11 md:h-14 rounded-lg px-3"
+              />
+              <textarea
+                placeholder="Message......."
+                className="px-3 border border-[#dadada] rounded-lg resize-none w-full pt-2 h-28"
+              ></textarea>
+              <button className="text-white bg-primary justify-center flex items-center w-[60%] md:w-[45%] lg:w-[35%]  mx-auto mt-6 py-2 text-lg capitalize rounded-lg">
+                submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
